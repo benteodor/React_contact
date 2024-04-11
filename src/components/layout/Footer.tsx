74738a1d73f14e4be1c4ab2b3a640be767d51630
Popa Teodor <teodor.popa96@gmail.com>
2024-04-11 16:07:25 +0300

    edits

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const Footer: FC = () => {
   return (
     <footer className='relative bottom-0 left-0 right-0 bg-gray-200 text-orange-600 h-12 flex justify-center items-center'>
-      <p>&copy; Adel Ansari</p>
+      <p>&copy; Teodor Popa</p>
     </footer>
   );
 };
